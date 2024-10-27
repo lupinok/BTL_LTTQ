e1dd7383017da03b7df959dc2b2ec1bda64d769b
minhquangxlpt <quang06112004@gmail.com>
2024-10-27 18:20:42 +0700

    q

--- DesignBTL/DesignBTL/Program.cs
+++ DesignBTL/DesignBTL/Program.cs
@@ -18,7 +18,7 @@ namespace DesignBTL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new TaiKhoan());
         }
     }
 }
