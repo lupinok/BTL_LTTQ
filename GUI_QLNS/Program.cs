2b8cfd519b435e27c231c54544e08b8622166edc
Vann Kite <146878611+Kiteya@users.noreply.github.com>
2024-11-01 08:23:37 -0700

    Taikhoan

--- GUI_QLNS/Program.cs
+++ GUI_QLNS/Program.cs
@@ -20,7 +20,7 @@ namespace GUI_QLNS
             // Kiểm tra kết nối trước
             if (TryConnectWithSavedSettings())
             {
-                Application.Run(new frmTaiKhoan());
+                Application.Run(new DangNhap());
             }
             else
             {
